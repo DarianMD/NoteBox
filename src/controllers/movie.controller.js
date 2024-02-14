ad81161701c4fdae579961700a6df4af16d756d3
DarianMD <darianmanasturean@gmail.com>
2024-02-14 21:00:25 +0100

    Filmafinity config, create files in tmp folder

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -1,13 +1,11 @@
 import movieSchema from '../models/movie.model.js'
 
-import urlRecoc from '../libs/movie.lib.js'
+import fetchData from '../libs/movie.lib.js'
 
 export const postmovie = (req, res) => {
     const {url} = req.body;
 
-    const typeHost = urlRecoc(url);
-
-    console.log(typeHost);
+    fetchData(url, null);
 
     res.send('postmovie');
 }
--- src/libs/movie.lib.js
+++ src/libs/movie.lib.js
@@ -1,33 +1,68 @@
-/*const https = require('https');
-const fs = require('fs');
-const cheerio = require('cheerio'); */
-
 import * as url from 'url';
-
-/*function fetchData(url, page, userID){
-    return new Promise((resolve, rejects) =>
-    {
-        https.get(url, function(res){
-            res.setEncoding('utf8');
-            let rawData = '';
-
-        }
-            
+import * as cheerio from 'cheerio';
+import * as fs from 'fs';
+import * as https from 'https';
 
 
-    
-)}*/
-
-
-
-export default function urlRecon(urlWeb){
-    
+export function urlHost(urlWeb){
 
     let dominio = url.parse(urlWeb, true);
     let host = dominio.host;
-
     dominio = host.replace(/^www\.|\.com$/gi, '');
-
     return dominio;
 }
 
+export default function fetchData(urlWeb, user_frontID) {
+
+    return new Promise((resolve, reject) => {
+
+        let dominio = url.parse(urlWeb, true);
+        let httpUrl, user_id, page;
+
+        
+        switch (urlHost(urlWeb)){
+            case "filmaffinity":
+                page = dominio.query.p;
+                dominio.query.p++;
+                user_id = dominio.query.user_id;
+                httpUrl = 'https://www.filmaffinity.com/es/userratings.php?user_id='+ dominio.query.user_id+'&p='+ dominio.query.p;
+                console.log(httpUrl);
+                break;
+            case "imdb":
+                console.log("Es imdb");
+                break;
+             case "letterboxd":
+                console.log("Es letterboxd");
+                break;               
+        }
+        
+
+
+        https.get(httpUrl, function(res) {
+            console.log(res.statusCode);
+            if (res.statusCode === 404) {
+                console.log("Error 404: No se encontró la página");
+                resolve(); // Resuelve la promesa para salir del bucle
+                return;
+            }
+        
+            res.setEncoding('utf8');
+            let rawData = '';
+            const file = fs.createWriteStream('./src/tmp/'+urlHost(urlWeb) + '_' + user_id + '_' + page + '.html'); // Crea un nuevo archivo
+
+            res.on('data', function(chunk) {
+                rawData += chunk;
+                file.write(chunk); // Escribir los datos en el archivo
+            });
+
+            res.on('end', function() {
+                file.end(); // Cerrar el archivo cuando se complete la respuesta
+            });
+            
+
+
+        }).on('error', function(err) {
+            reject(err); // Rechaza la promesa en caso de error
+        });
+    });
+}
